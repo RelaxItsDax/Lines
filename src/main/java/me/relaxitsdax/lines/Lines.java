f201c266c571d2f793be9fa08e481f012fb8e353
RelaxItsDax <relaxitsdax@gmail.com>
2023-02-04 12:47:11 -0500

    Initial commit

---
+++ src/main/java/me/relaxitsdax/lines/Lines.java
@@ -0,0 +1,18 @@
+package me.relaxitsdax.lines;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Lines extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
